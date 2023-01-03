067caac22262b4554ad96f865f95cb5f35e0b1ff
Francisco Miguel Sena <fmesena@gmail.com>
2023-01-03 02:03:45 +0000

    add new state for symbolic interpreter

--- lib/SState.ml
+++ lib/SState.ml
@@ -1,1 +1,3 @@
-type t = SStore.t * (Program.stmt list) * Callstack.t * (Expression.expr list)
+type path_condition = Expression.expr list
+
+type t = SStore.t * (Program.stmt list) * Callstack.t * path_condition
