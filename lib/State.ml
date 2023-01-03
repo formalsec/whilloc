3e41049373b19fd00268f858ecfbe05e59754876
Francisco Miguel Sena <fmesena@gmail.com>
2023-01-03 23:02:22 +0000

    move current statement to type State (towards more compact code)

--- lib/State.ml
+++ lib/State.ml
@@ -1,8 +1,11 @@
-type t = Store.t * (Program.stmt list) * Callstack.t
+type t = Program.stmt * (Program.stmt list) * Store.t * Callstack.t
+
+let get_current_stmt (state : t) : Program.stmt =
+  let stmt,_,_,_=state in stmt
 
 let get_continuation (state : t) : Program.stmt list =
-  let _,cont,_=state in cont
+  let _,cont,_,_=state in cont
 
 let push_statements (state : t) (to_add : Program.stmt list) : t =
-  let st,stmts,cs = state in
-  (st, to_add@stmts,cs)
+  let stmt,stmts,st,cs = state in
+  (stmt,to_add@stmts,st,cs)
