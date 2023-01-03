22642acd6fbc7c242ef78b563bb55e1f254d0739
Francisco Miguel Sena <fmesena@gmail.com>
2023-01-03 23:08:00 +0000

    change Return constructor to hold an expression instead of a value

--- lib/Outcome.ml
+++ lib/Outcome.ml
@@ -1,12 +1,12 @@
-type t = Cont | Return of Expression.value | AssumeF | Error
+type t = Cont | Return of Expression.expr | AssumeF | Error
 
 let string_of_outcome (o : t) : string =
   let prefix = "OUTCOME: " in
   match o with
-  | Cont     -> prefix ^ "Cont"
+  | Cont     -> prefix ^ "Continue"
   | Error    -> prefix ^ "Assertion evaluated to false"
   | AssumeF  -> prefix ^ "Assumption evaluated to false"
-  | Return v -> prefix ^ "Returned " ^ (Expression.string_of_value v)
+  | Return e -> prefix ^ "Returned " ^ (Expression.string_of_expression e)
 
 let should_halt (o: t) : bool =
   match o with
