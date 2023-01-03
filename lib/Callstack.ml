fee57b73bc3bb2781f35c09feb220446f5ae3e38
Francisco Miguel Sena <fmesena@gmail.com>
2023-01-03 23:03:42 +0000

    improve error messages

--- lib/Callstack.ml
+++ lib/Callstack.ml
@@ -4,10 +4,10 @@ type t = frame list
 
 let top (cs : t): frame =
   match cs with
-  | [] -> failwith "InternalError: Tried to peek at the top of an empty stack"
+  | [] -> failwith "InternalError: Tried to peek from an empty stack"
   | top :: _ -> top
 
 let pop (cs : t) : t =
   match cs with
-  | [] -> failwith "InternalError: Tried to retrieve an empty stack"
+  | [] -> failwith "InternalError: Tried to pop from an empty stack"
   | _ :: t -> t
