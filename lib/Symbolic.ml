0fe9cd2c44e6da67bbfda08b4c1aa418cc85ebd2
Francisco Miguel Sena <fmesena@gmail.com>
2023-01-05 22:03:42 +0000

    working on the symbolic interpreter. should support symbolic var decl and evaluation of symb exprs

--- lib/Symbolic.ml
+++ lib/Symbolic.ml
@@ -1,8 +1,25 @@
 open Program
 open Expression
 open Outcome
+open Search
 
-let eval_unop_expr (op : uop) (v : value) : expr =
+let rec is_symbolic_expression (st : SStore.t) (e : expr) : bool =
+  match e with
+  | Val _     -> false
+  | SymbVal _ -> true
+  | Var x     -> SStore.get st x |> get_expr_from_opt |> is_symbolic_expression st 
+  | UnOp  (_, e)    -> is_symbolic_expression st e
+  | BinOp (_,e1,e2) -> is_symbolic_expression st e1 || is_symbolic_expression st e2
+
+let rec simplify_symb_expr (st : SStore.t) (e : expr) : expr =
+  match e with
+  | Val _     -> e
+  | SymbVal _ -> e
+  | Var x     -> get_expr_from_opt ( SStore.get st x ) 
+  | UnOp  (op, e)    -> UnOp (op, simplify_symb_expr st e)
+  | BinOp (op,e1,e2) -> BinOp(op, simplify_symb_expr st e1, simplify_symb_expr st e2)
+
+let eval_unop_expr (op : uop) (v : value) : Expression.expr =
   Val (
   match op with
   | Neg -> neg v
@@ -10,7 +27,7 @@ let eval_unop_expr (op : uop) (v : value) : expr =
   | Abs -> abs v
   | StringOfInt -> stoi v)
 
-let eval_binop_expr (op : bop) (v1 : value) (v2 : value) : expr =
+let eval_binop_expr (op : bop) (v1 : value) (v2 : value) : Expression.expr =
   Val (
   match op with
   | Plus    -> plus   (v1, v2)
@@ -31,13 +48,12 @@ let eval_binop_expr (op : bop) (v1 : value) (v2 : value) : expr =
   | ShiftL  -> shl    (v1, v2)
   | ShiftR  -> shr    (v1, v2))
 
-let rec eval_expression (st : SStore.t) (e : expr) : expr = 
+let rec eval_expression (st : SStore.t) (e : Expression.expr) : Expression.expr = 
 
-  (* If the expression contains symbolic values we can't reduce it to a value, so it is returned as is.
-     In the future, I will write a 'simplify' function that takes advantage of the properties of the operators of our language (commutativity, etc) *)
-  if is_symbolic_expression e then e (*if is_symbolic_expression e then simplify_symb_expr e  (#e.g. 2x̂+2-x̂+1 --> 3x̂+3) *)
+  (* We can't reduce the expression 'e' to a value if it contains symbolic variables, so we just replace each symbolic variable with its symbolic value *)
+  if is_symbolic_expression st e then simplify_symb_expr st e
   
-  (* If the expression does not contains symbolic values, we can reduce it until it becomes a value *)
+  (* If the expression does not contains symbolic variables, we can reduce it until it becomes a value *)
   else
 
   match e with
@@ -45,7 +61,7 @@ let rec eval_expression (st : SStore.t) (e : expr) : expr =
     | Var x ->
         let value = SStore.get st x in
         (match value with
-        | None    -> failwith ("NameError: name " ^ x ^ " is not defined")
+        | None    -> failwith ("NameError: Symbolic, name " ^ x ^ " is not defined")
         | Some v  -> v) (*here, 'v' will always be an expr.Val *)
 
     | Val v -> Val v
@@ -53,10 +69,10 @@ let rec eval_expression (st : SStore.t) (e : expr) : expr =
     | UnOp  (op, e)      -> eval_expression st e |> get_value_from_expr |> eval_unop_expr op
     | BinOp (op, e1, e2) -> eval_binop_expr op (eval_expression st e1 |> get_value_from_expr) (eval_expression st e2 |> get_value_from_expr)
 
-    | SymbVal x -> failwith ("InternalError: tried to evaluate an expression consisting of a symbolic value \'" ^ x ^ "\'")
+    | SymbVal x -> failwith ("InternalError: Symbolic, tried to reduce an expression consisting of a symbolic value \'" ^ x ^ "\'")
 
 
-let step (prog : program) (state : SState.t) : SState.t * Outcome.t (*list? why?*) = 
+let step (prog : program) (state : SState.t) : Return.t list = 
 
   let s,cont,store,cs,pc = state in
 
@@ -64,43 +80,44 @@ let step (prog : program) (state : SState.t) : SState.t * Outcome.t (*list? why?
 
   | Skip | Clear ->
     (match cont with
-    | []     ->        state     , Cont
-    | h :: t -> (h, t, store, cs, pc), Cont)
+    | []     -> [        state         , Cont  ]
+    | h :: t -> [ (h, t, store, cs, pc), Cont  ])
   
-  | Sequence (s1::s2) -> (s1, s2@cont, store, cs, pc), Cont
+  | Sequence (s1::s2) -> [ (s1, s2@cont, store, cs, pc), Cont ]
 
   | Assign (x,e) ->
       SStore.set store x (eval_expression store e);
-      (Skip, cont, store, cs, pc), Cont
+      [ (Skip, cont, store, cs, pc), Cont ]
   
   | Symbol s ->
       let symb_val = make_symb_value s in
       SStore.set store s symb_val;
-      (Skip, cont, store, cs, pc), Cont
+      [ (Skip, cont, store, cs, pc), Cont ]
 
   | Print exprs ->
       let eval_exprs = List.map (eval_expression store) exprs in
-      let ()         = List.iter print_expression eval_exprs in
-      let ()         = print_endline "" in
-      (Skip, cont, store, cs, pc), Cont
+      let ()         = print_endline ">Program Print" in
+      let ()         = List.iter print_expression eval_exprs; print_endline "" in
+      [ (Skip, cont, store, cs, pc), Cont ]
   
   | FunCall (var,id,args) ->
-    let eval_args   = List.map (fun e -> eval_expression store e) args in
-    let function'   = Program.get_function id prog in
-    let params      = function'.args in
-    let var_vals    = try List.combine params eval_args
-                      with _ -> failwith ("TypeError: argument arity mismatch when calling " ^ id) in
-    let func_frame  = SStore.create_store var_vals in
-    let cs'         = (SCallstack.Intermediate (store, cont, var) ) :: cs in
-    (function'.body, [], func_frame, cs', pc), Cont
+      let eval_args   = List.map (fun e -> eval_expression store e) args in
+      let function'   = Program.get_function id prog in
+      let params      = function'.args in
+      let var_vals    = try List.combine params eval_args
+                        with _ -> failwith ("TypeError: Symbolic, argument arity mismatch when calling " ^ id) in
+      let func_frame  = SStore.create_store var_vals in
+      let cs'         = (SCallstack.Intermediate (store, cont, var) ) :: cs in
+      [ (function'.body, [], func_frame, cs', pc), Cont ]
 
   | Return e ->
-    let frame = SCallstack.top cs in
-    let cs'   = SCallstack.pop cs in
-      (match frame with
-      | SCallstack.Intermediate (store',rest,var)  -> (SStore.set store' var e;
-                                                      (Skip, rest, store', cs', pc), Cont)
-      | SCallstack.Toplevel  -> (Skip, cont, store, cs', pc), Return e)
+      let v     = eval_expression store e in
+      let frame = SCallstack.top cs in
+      let cs'   = SCallstack.pop cs in
+        (match frame with
+        | SCallstack.Intermediate (store',rest,var) ->  SStore.set store' var v;
+                                                        [ (Skip, rest, store', cs', pc), Cont ]
+        | SCallstack.Toplevel  -> [ (Skip, cont, store, cs', pc), Return v ])
 
   | IfElse (e, s1, s2) ->
       let e = eval_expression store e in
@@ -122,9 +139,9 @@ let step (prog : program) (state : SState.t) : SState.t * Outcome.t (*list? why?
             ( Skip,cont,store,cs,pc ), Cont
       *)
       if is_true (get_value_from_expr e) then
-        (s1, cont, store, cs, pc), Cont
+        [ (s1, cont, store, cs, pc), Cont ]
       else
-        (s2, cont, store, cs, pc), Cont
+        [ (s2, cont, store, cs, pc), Cont ]
 
   | While (e,body) as while_stmt ->
       (*
@@ -135,7 +152,7 @@ let step (prog : program) (state : SState.t) : SState.t * Outcome.t (*list? why?
         [ ( Skip,cont,store,cs,pc ), Cont ]
       *)
       let unfold_loop = IfElse (e, Sequence ( (sequence_content body)@[while_stmt] ), Skip) in
-      (unfold_loop, cont, store, cs, pc), Cont
+      [ (unfold_loop, cont, store, cs, pc), Cont ]
 
   (* (pc ∧ e) SAT   means that there exists a concretization of symbolic values that makes the expression evaluate to true  *)
   (* (pc ∧ ê) SAT   means that there exists a concretization of symbolic values that makes the expression evaluate to false *)
@@ -150,10 +167,10 @@ let step (prog : program) (state : SState.t) : SState.t * Outcome.t (*list? why?
     No : Continue
     IDK: ?
   *)
-  | Assert _ -> 
-      let v = (Boolean true) in 
-      if is_true v then (Skip, cont, store, cs, pc), Cont
-      else              (Skip, cont, store, cs, pc), Error
+  | Assert e -> 
+      let v = get_value_from_expr (eval_expression store e) in (*FIXME temporary line of code*)
+      if is_true v then [ (Skip, cont, store, cs, pc), Cont  ]
+      else              [ (Skip, cont, store, cs, pc), Error ]
 
   (*
     Semantic of assume(e): assume(e) means that we are assuming that at least one concretization of symbolic values makes the expression 'e' evaluate to true, thus,
@@ -163,33 +180,31 @@ let step (prog : program) (state : SState.t) : SState.t * Outcome.t (*list? why?
     No : Continue
     IDK: ?
   *)
-  | Assume _ ->
-      let v = (Boolean true) in 
-      if is_true v then (Skip, cont, store, cs, pc), Cont
-      else              (Skip, cont, store, cs, pc), Error
+  | Assume e ->
+      let v = get_value_from_expr (eval_expression store e) in (*FIXME temporary line of code*)
+      if is_true v then [ (Skip, cont, store, cs, pc), Cont  ]
+      else              [ (Skip, cont, store, cs, pc), AssumeF ]
 
-  | Sequence [] -> failwith "InternalError: tried to evaluate an empty Sequence"
+  | Sequence [] -> failwith "InternalError: Symbolic, tried to evaluate an empty Sequence"
 
-let rec eval (gas : int) (prog : program) (state : SState.t) (out : Outcome.t) : SState.t * Outcome.t = 
+let rec search (gas : int) (prog : program) (states : SState.t list) (rets : Return.t list) : Return.t list = 
 
-  if gas=0 then state,out
-  
-  else
+  if gas=0 || states=[] then rets else
 
-  let state',out' = step prog state in
-  let (stmt',cont',_,_,_) = state' in
+  let state, states' = pick states in
+  let branches       = step prog state in
 
-  match stmt',cont',out' with
-  | Skip, [], Cont     -> failwith "BadProgram: functions should always return a value"
-  | _,    _ , Error    -> state',out'
-  | _,    _ , AssumeF  -> state',out'
-  | _,    _ , Return _ -> state',out'
-  | _                  -> eval (gas-1) prog state' out
+  let branches_final, branches_cont = List.partition is_final branches in
 
-let interpret (prog : program) (main_id : string) : Outcome.t =
+  let next_states_cont,_  = List.split branches_cont in
+
+  search (gas-1) prog (join next_states_cont states') ( branches_final @ rets )
+
+
+let interpret (prog : program) (main_id : string) : Return.t list = 
   let main  = get_function main_id prog in
-  let tank  = 10000 in 
+  let tank  = Configs.tank in 
   let pathc = [Val (Boolean true)] in
   let state = (Skip, [main.body], SStore.create_empty_store 100, [SCallstack.Toplevel], pathc) in
-  let _, o  = eval tank prog state Cont in
-  o
+  let r  = search tank prog [state] [] in
+  r
