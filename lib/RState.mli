cbabfc6a3594801dc9046ca2ecf2d21aba3c1a04
José Santos <josesantos@Joses-MacBook-Pro.local>
2023-01-20 16:16:34 +0000

    refactoring

---
+++ lib/RState.mli
@@ -0,0 +1,19 @@
+module type M = sig 
+  
+  (* state type *)
+  type t 
+
+  (* value type *)
+  type vt 
+
+  val get_conf : t -> Program.stmt list
+
+  val set : t -> string -> vt -> unit 
+  
+  val eval_expression : t -> Expression.t -> vt 
+
+  val make_symbol : t -> string -> vt 
+
+  val val_to_string : t -> vt -> string 
+
+end 
