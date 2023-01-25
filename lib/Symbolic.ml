ce2283d707040de61e81b9c10b749a8cfc86658a
Francisco Miguel Sena <fmesena@gmail.com>
2023-01-25 03:23:51 +0000

    add tree construction; fix bug on 'Return'

--- lib/Symbolic.ml
+++ lib/Symbolic.ml
@@ -3,6 +3,7 @@ open Expression
 open Outcome
 open Search
 open Encoding
+open BinaryTree
 
 let is_symbolic_value (v : value) : bool =
   match v with
@@ -111,8 +112,9 @@ let step (prog : program) (state : SState.t) : Return.t list =
       let frame = SCallstack.top cs in
       let cs'   = SCallstack.pop cs in
         (match frame with
-        | SCallstack.Intermediate (store',rest,var) ->  SStore.set store' var v;
-                                                        [ (Skip, rest, store', cs', pc), Cont ]
+        | SCallstack.Intermediate (store',rest,var) ->  let store'' = Hashtbl.copy store' in (* necessary when the function body branches and returns different values *)
+                                                        SStore.set store'' var v;
+                                                        [ (Skip, rest, store'', cs', pc), Cont ]
         | SCallstack.Toplevel  -> [ (Skip, cont, store, cs', pc), Return v ])
 
   | IfElse (e, s1, s2) when is_symbolic_expression store e->
@@ -124,8 +126,8 @@ let step (prog : program) (state : SState.t) : Return.t list =
       
       let store' = if then_guard && else_guard then Hashtbl.copy store else store in
 
-      let then_branch = if then_guard then [ (s1, cont, store , cs, e' ::pc ), Cont ] else [] in
-      let else_branch = if else_guard then [ (s2, cont, store', cs, e''::pc ), Cont ] else [] in
+      let then_branch = if then_guard then [ (s1, cont, store , cs, e' ::pc ), Cont ] else [ ] in
+      let else_branch = if else_guard then [ (s2, cont, store', cs, e''::pc ), Cont ] else [ ] in
 
       then_branch @ else_branch
 
@@ -142,10 +144,10 @@ let step (prog : program) (state : SState.t) : Return.t list =
       let guard_t = is_sat (e' ::pc ) in
       let guard_f = is_sat (e''::pc ) in
 
-      let store' = if guard_t && guard_f then Hashtbl.copy store else store in
+      let store' = if guard_t && guard_f then (Hashtbl.copy store) else store in
 
-      let body_branch = if guard_t then [ ( body, while_stmt::cont, store , cs, e' ::pc ), Cont ] else [] in
-      let skip_branch = if guard_f then [ ( Skip, cont            , store', cs, e''::pc ), Cont ] else [] in
+      let body_branch = if guard_t then [ ( body, while_stmt::cont, store , cs, e' ::pc ), Cont ] else [ ] in
+      let skip_branch = if guard_f then [ ( Skip, cont            , store', cs, e''::pc ), Cont ] else [ ] in
 
       body_branch @ skip_branch
 
@@ -182,11 +184,10 @@ let step (prog : program) (state : SState.t) : Return.t list =
     No : AssumeF
   *)
   | Assume e when is_symbolic_expression store e ->
-    let e'     = simplify_symb_expr store e in
-    print_endline (string_of_expression e');
-    let is_sat = is_sat (e'::pc) in
-    if is_sat then [ (Skip, cont, store, cs, e'::pc), Cont    ]
-    else           [ (Skip, cont, store, cs,     pc), AssumeF ]
+      let e'     = simplify_symb_expr store e in
+      let is_sat = is_sat (e'::pc) in
+      if is_sat then [ (Skip, cont, store, cs, e'::pc), Cont    ]
+      else           [ (Skip, cont, store, cs,     pc), AssumeF ]
 
   | Assume e ->
       let v = get_value_from_expr (eval_expression store e) in
@@ -195,26 +196,31 @@ let step (prog : program) (state : SState.t) : Return.t list =
 
   | Sequence [] -> failwith "InternalError: Symbolic, tried to evaluate an empty Sequence"
 
-let rec search (gas : int) (prog : program) (states : SState.t list) (rets : Return.t list) : Return.t list = 
+let rec search (gas : int) (prog : program) (states : SState.t list) (returns : Return.t list) (tree : Program.stmt BinaryTree.t) : Return.t list * Program.stmt BinaryTree.t = 
 
-  if gas=0 || states=[] then rets else
+  if gas=0 || states=[] then returns,tree else
 
   let state, states' = pick_head states in
   let branches       = step prog  state in
 
+  let tree' = add_left tree (SState.get_current_stmt state) in
+
   (*print_endline (SState.string_of_sstate state);*)
 
   let branches_final, branches_cont = List.partition is_final branches in
+  let states_cont   , _             = List.split branches_cont in
 
-  let next_states_cont,_  = List.split branches_cont in
-
-  search (gas-1) prog (join_push next_states_cont states') ( branches_final @ rets )
+  search (gas-1) prog (join_enqueue states_cont states') ( branches_final @ returns ) tree'
 
 
 let interpret (prog : program) (main_id : string) : Return.t list = 
   let main  = get_function main_id prog in
-  let tank  = Parameters.tank in 
+  let tank  = Parameters.tank in
+  let store = SStore.create_empty_store 100 in
+  let cs    = [SCallstack.Toplevel] in
   let pathc = [Val (Boolean true)] in
-  let state = (Skip, [main.body], SStore.create_empty_store 100, [SCallstack.Toplevel], pathc) in
-  let r  = search tank prog [state] [] in
+  let state = (Skip, [main.body], store, cs, pathc) in
+  let r,t = search tank prog [state] [] Nil in
+  print_tree Program.string_of_stmt t;
+  print_endline (to_graphviz Program.string_of_stmt t);
   r
