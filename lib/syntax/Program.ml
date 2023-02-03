d17e483bd3e8473b668937761ef70a54fdfdd05a
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-03 02:49:57 +0000

    updates program module wrt all the recent changes

--- lib/syntax/Program.ml
+++ lib/syntax/Program.ml
@@ -1,15 +1,15 @@
 type stmt = Skip
-        | Assign      of string * Expression.expr
-        | Sequence    of stmt list
-        | FunCall     of string * string * Expression.expr list
-        | IfElse      of Expression.expr * stmt * stmt
-        | While       of Expression.expr * stmt
-        | Return      of Expression.expr
-        | Assert      of Expression.expr
-        | Assume      of Expression.expr
-        | Clear
-        | Print       of Expression.expr list
-        | Symbol      of string * string
+          | Assign      of string * Expression.t
+          | Sequence    of stmt list
+          | FunCall     of string * string * Expression.t list
+          | IfElse      of Expression.t * stmt * stmt
+          | While       of Expression.t * stmt
+          | Return      of Expression.t
+          | Assert      of Expression.t
+          | Assume      of Expression.t
+          | Clear
+          | Print       of Expression.t list
+          | Symbol      of string * string
 
 type func = {
   id   : string;
@@ -21,39 +21,24 @@ type program = (string, func) Hashtbl.t
 
 (* Helper functions *)
 
-let make_fresh_symb_generator (pref : string) : (unit -> string) =
-  let count = ref 1 in
-  fun () -> let x = !count in
-    count := x+1; pref ^ (string_of_int x)
-
-let generate_fresh_var = make_fresh_symb_generator "§_"
-
-let make_symb_value (name : string) : Expression.value = (*X̂x̂*)
-  SymbVal ( generate_fresh_var() ^ "__" ^ name)
-
-let sequence_content (s : stmt) : stmt list =
-  match s with
-  | Sequence seq -> seq
-  | _            -> failwith "InternalError: tried to retrieve a statements carried along with a \"Sequence\" constructor from a non sequence constructor" 
-
-let get_function (id : string) (p : program) : func =
-  try Hashtbl.find p id
-  with _ -> failwith ("NameError: name " ^ id ^ " is not defined")
-
+let get_function (id : string) (prog : program) : func =
+  try Hashtbl.find prog id
+  with _ -> failwith ("NameError: Program.get_function, name " ^ id ^ " is not defined")
+            (* exception NameError of string * string (id, message to be shown). this is also used in Store.ml *)
 let rec string_of_stmt (s : stmt) : string =
   match s with
-  | Skip -> "Skip\n"
-  | Assign (x,e) -> "Assignment: " ^ x ^ "=" ^ (Expression.string_of_expression e) ^ "\n"
-  | Sequence s -> "Sequence:\n  " ^ String.concat "  " (List.map string_of_stmt s)
-  | FunCall (x,f,args) -> "FunCall: " ^ x ^ "=" ^ f ^ "(" ^ String.concat ", " (List.map (fun e -> Expression.string_of_expression e) args) ^ ")\n"
-  | IfElse (expr, s1, s2) -> "If (" ^ (Expression.string_of_expression expr) ^ ")\n  " ^ string_of_stmt s1 ^ "\nElse\n  " ^ string_of_stmt s2 ^ "\n"
-  | While (expr, s) -> "While (" ^ Expression.string_of_expression expr ^ ")\n   " ^ string_of_stmt s ^ "\n"
-  | Return e     -> "Return: " ^ "return " ^ Expression.string_of_expression e ^ "\n"
-  | Assert e     -> "Assert: " ^ Expression.string_of_expression e ^ ")\n"
-  | Assume e     -> "Assume: " ^ Expression.string_of_expression e ^ ")\n"
+  | Skip         -> "Skip"
   | Clear        -> "Clear\n"
-  | Print  es    -> "Print: "  ^ String.concat ", " (List.map (fun e -> Expression.string_of_expression e) es) ^ ")\n"
-  | Symbol (s,v) -> "Symbol declaration: " ^ s ^ "=" ^ v ^ "\n"
+  | Assign (x,e) -> "Assignment: " ^ x ^ "=" ^ (Expression.string_of_expression e)
+  | Symbol (s,v) -> "Symbol declaration: name=" ^ s ^ ", value=§__" ^ v
+  | Sequence s   -> "Sequence:\n  " ^ String.concat "  " (List.map string_of_stmt s)
+  | Return e     -> "Return: " ^ Expression.string_of_expression e
+  | Assert e     -> "Assert: " ^ Expression.string_of_expression e
+  | Assume e     -> "Assume: " ^ Expression.string_of_expression e
+  | Print  exprs -> "Print:  " ^ String.concat ", " (List.map (fun e -> Expression.string_of_expression e) exprs)
+  | IfElse  (expr, s1, s2) -> "If (" ^ (Expression.string_of_expression expr) ^ ")\n  " ^ string_of_stmt s1 ^ "\nElse\n  " ^ string_of_stmt s2
+  | FunCall (x,f,args)     -> "Function Call: " ^ x ^ "=" ^ f ^ "(" ^ String.concat ", " (List.map (fun e -> Expression.string_of_expression e) args)
+  | While   (expr, s)      -> "While (" ^ Expression.string_of_expression expr ^ ")\n   " ^ string_of_stmt s
 
 let print_statement (s : stmt) : unit =
   s |> string_of_stmt |> print_endline
