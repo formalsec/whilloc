ee03ffa6a13f7285f95fb94672dd013f93dee0b7
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-03 02:47:16 +0000

    Return now yields a string instead of an expression

--- lib/semantics/Outcome.ml
+++ lib/semantics/Outcome.ml
@@ -1,14 +1,13 @@
-type t = Cont | Return of Expression.expr | AssumeF | Error (* of (string, Expression.value) Hashtbl *)
+type t = Cont | Return of string | AssumeF | Error (* of (string, Expression.value) Hashtbl *)
 
 let string_of_outcome (o : t) : string =
-  "OUTCOME: " ^ 
   match o with
   | Cont     -> "Continue"
   | Error    -> "Assertion evaluated to false"
   | AssumeF  -> "Assumption evaluated to false"
-  | Return e -> "Returned " ^ (Expression.string_of_expression e)
+  | Return e -> "Returned " ^ e
 
-let should_halt (o: t) : bool =
+let should_halt (o : t) : bool =
   match o with
   | Error | AssumeF -> true
   | _ -> false
