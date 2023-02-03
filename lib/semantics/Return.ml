5f01d67da9b3c9015009721df4fde384963d27cb
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-03 02:48:16 +0000

    new parametric return type, new to_string function

--- lib/semantics/Return.ml
+++ lib/semantics/Return.ml
@@ -1,8 +1,24 @@
-type t = SState.t * Outcome.t
+type 'v t = 'v State.t * Outcome.t
 
-let string_of_return (ret : t) : string =
-  let (_,_,st,_,pc), out = ret in
-  "\n# RETURN #\nPC:      " ^ (PathCondition.string_of_pathcondition pc) ^ "\n" ^ (Outcome.string_of_outcome out) ^ "\nSTORE:\n" ^ (SStore.string_of_store st) ^ "\n"
+let string_of_return (to_string : 'v -> string) (ret : 'v t) : string =
+  let (_,_,st,_,pc), out = ret in "\n#RETURN:\n" ^
+  " -PC:      " ^ (PathCondition.string_of_pathcondition to_string pc) ^ "\n" ^
+  " -OUTCOME: " ^ (Outcome.string_of_outcome out)            ^ "\n" ^
+  " -STORE:"    ^ (Store.string_of_store to_string st)       ^ "\n"
 
-  let print_return (ret : t) : unit =
-    string_of_return ret |> print_endline
+let print (to_string : 'v -> string) (ret : 'v t) : unit =
+  string_of_return to_string ret |> print_endline
+
+(*
+type 'v t = 'v Store.t * Outcome.t * PathCondition.t
+type 'v t =    Outcome.t * PathCondition.t
+
+let string_of_return (store_str : 'v -> string) (out_str : 'v -> string) (ret : 'v t) : string =
+  let store,out,pc = ret in "\n#RETURN #\n" ^
+  " PC:      " ^ (PathCondition.string_of_pathcondition pc)   ^ "\n" ^
+  " Outcome: " ^ (Outcome.string_of_outcome out_str out)      ^ "\n" ^
+  " STORE:"    ^ (Store.string_of_store store_str store)      ^ "\n"
+
+let print_return (store_str : 'v -> string) (out_str : 'v -> string) (ret : 'v t) : unit =
+  string_of_return store_str out_str ret |> print_endline
+*)
