2688595d092649a691432ed6dd1b6db66c454d3a
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-06 20:24:48 +0000

    asserts distinct function ids in input program

--- bin/main.ml
+++ bin/main.ml
@@ -1,30 +1,20 @@
 open Lib
 
-let create_program (funcs : Program.func list) : Program.program = (*TODO assert pairwise distinct function ids*)
-  let prog = Hashtbl.create 100 in
-  List.iter (fun (f : Program.func) -> Hashtbl.replace prog f.id f) funcs;
+let create_program (funcs : Program.func list) : Program.program =
+  let prog    = Hashtbl.create Parameters.size in
+  let replace =
+    fun (prog : Program.program) (func : Program.func) ->
+      let f = (Hashtbl.find_opt prog func.id) in
+      match f with
+        | None   -> Hashtbl.replace prog func.id func
+        | Some _ -> failwith "BadProgram: function names must pairwise distinct" in
+  List.iter (fun func -> replace prog func) funcs;
   prog
 
 let parse_program (str : string) : Program.func list =
   let lexbuf = Lexing.from_string str in
   let funcs  = Parser.program_target Lexer.read lexbuf in
   funcs
-
-(*
-let print_position outx lexbuf =
-  let pos = lexbuf.lex_curr_p in
-  fprintf outx "%s:%d:%d" pos.pos_fname
-    pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
-
-let parse_with_error lexbuf =
-  try Parser.prog Lexer.read lexbuf with
-  | SyntaxError msg ->
-    fprintf stderr "%a: %s\n" print_position lexbuf msg;
-    None
-  | Parser.Error ->
-    fprintf stderr "%a: syntax error\n" print_position lexbuf;
-    exit (-1)   
-*)
 
 let read_file (fname : string) : string =
   let ch = open_in fname in
@@ -33,9 +23,8 @@ let read_file (fname : string) : string =
   str_file
 
 let main =
-  let filename    = Sys.argv.(1) in
-  let program     = filename |> read_file |> parse_program |> create_program in
-
+  let filename = Sys.argv.(1) in
+  let program  = filename |> read_file |> parse_program |> create_program in
   let module I = MakeInterpreter.M (EvalSymbolic.M) (DFS.M) in
   let prog_return = I.interpret program in
   List.iter (Return.print Expression.string_of_expression) prog_return
