b9f23e5523d3c3fff33f4bf46f2755a964b3c087
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-06 21:36:28 +0000

    main now asks for options in console

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,11 @@
 open Lib
 
+let file = ref ""
+let mode = ref ""
+let out = ref ""
+let verbose = ref false
+
+
 let create_program (funcs : Program.func list) : Program.program =
   let prog    = Hashtbl.create Parameters.size in
   let replace =
@@ -22,35 +28,37 @@ let read_file (fname : string) : string =
   close_in ch;
   str_file
 
+let arguments () =
+
+  let usage_msg = "Usage: -i <path> -mode <c/p> -o <path> [-v|-s] -h <path> [--parse]" in
+  Arg.parse
+    [
+      ("-i"   , Arg.String (fun f -> file := f ), "Input file");
+      ("-m"   , Arg.String (fun m -> mode := m ), "Mode to run: c - Concrete / s - Symbolic ");
+      ("-o"   , Arg.String (fun o -> out  := o ), "Output file");
+      ("-v"   , Arg.Set verbose, "Verbose")
+    ]
+    (fun s -> Printf.printf "Ignored Argument: %s" s)
+    usage_msg
+
 let main =
-  let filename = Sys.argv.(1) in
-  let program  = filename |> read_file |> parse_program |> create_program in
-  let module I = MakeInterpreter.M (EvalSymbolic.M) (DFS.M) in
-  let prog_return = I.interpret program in
-  List.iter (Return.print Expression.string_of_expression) prog_return
+  print_string "\n=====================\n\tÆnima\n=====================\n\n";
+  arguments();
 
-(*
-module I = MakeInterpreter.M (EvalSymbolic) (DFS)
-module I = MakeInterpreter.M (EvalConcrete) (DFS)
-I.interpret (...)
+  if (!file = "" && !mode = "" && !out = "") then (print_string "\nNo option selected. Use -h\n";)
+  else if (!file = "") then (print_string "No input file. Use -i\n\n=====================\n\tFINISHED\n=====================\n";)
+  else if (!mode = "") then (print_string "No mode selected. Use -mode\n\n=====================\n\tFINISHED\n=====================\n";)
+  else if (!out  = "") then (print_string "No output file. Use -o\n\n=====================\n\tFINISHED\n=====================\n";)
+  
+  else
 
-module I = MakeInterpreter.M (EvalSymbolic) (StateSymbolic) (DFS)
-module I = MakeInterpreter.M (EvalConcrete) (StateConcrete) (DFS)
-I.interpret (...)
-*)   
+  let program  = !file |> read_file |> parse_program |> create_program in
+  let module S = MakeInterpreter.M (EvalSymbolic.M) (DFS.M) in
+  let module C = MakeInterpreter.M (EvalConcrete.M) (DFS.M) in
 
-  (*
-  let ()          = print_endline "\n>>> Symbolic Interpreter" in
-  let prog_return = Symbolic.interpret program in
-  let ()          = List.iter Return.print_return prog_return in
-
-  let ()          = print_endline "\n>>> Smallstep Interpreter" in
-  let prog_state  = Smallstep.interpret program in
-  let ()          = print_endline (Outcome.string_of_outcome prog_state ^ "\n") in
-
-  let ()          = print_endline "\n>>> Bigstep Interpreter" in
-  let prog_state  = Bigstep.interpret program in
-                    print_endline (Outcome.string_of_outcome prog_state ^ "\n")
-  *)
+  match !mode with
+    | "c" -> List.iter (Return.print Value.string_of_value)           (C.interpret program)
+    | "s" -> List.iter (Return.print Expression.string_of_expression) (S.interpret program)
+    | _   -> invalid_arg "Unknown provided mode. Available modes are:\n  s : for symbolic interpretation\n  c : for concrete interpretation\n"
 
 let _ = main
