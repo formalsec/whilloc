54072c7f5fabcb897e2f93cd5773f452947515da
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-15 05:34:06 +0000

    add helper functions

--- lib/semantics/Return.ml
+++ lib/semantics/Return.ml
@@ -6,5 +6,11 @@ let string_of_return (to_string : 'v -> string) (ret : 'v t) : string =
   " -Outcome   : " ^ (Outcome.string_of_outcome out)                      ^ "\n" ^
   " -Store     : " ^ (Store.string_of_store to_string st)                 ^ "\n"
 
+let outcome (ret : 'v t) : Outcome.t =
+  let _, out = ret in out
+
+let pc (ret : 'v t) =
+  let (_,_,_,_,pc), _ = ret in pc
+
 let print (to_string : 'v -> string) (ret : 'v t) : unit =
   string_of_return to_string ret |> print_endline
