925709a5efb8fe3eb75768ed60c89b4a0a977025
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-22 22:34:51 +0000

    cleaning

--- lib/interpreters/Interpreter.mli
+++ lib/interpreters/Interpreter.mli
@@ -1,9 +1,4 @@
 module type M = sig
     type t
-    (*
-    Maybe add here the epsilon_map, in the symbolic case do Encoding.get_model and in the concrete case the model is the current store
-    val epsilon : string -> Value.t
-    The domain of this function coincides with the set of symbolic variable created during the execution of the program
-    *)
     val interpret : Program.program -> t Return.t list
 end
