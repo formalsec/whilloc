9a5708da1a71d795030e6971a2e13583df063918
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-22 22:36:55 +0000

    adds negation of path conditions, renames variables, cleans code

--- lib/datastructures/PathCondition.ml
+++ lib/datastructures/PathCondition.ml
@@ -1,17 +1,21 @@
-type 'v t = 'v list (* a conjunction of expressions *)
+type 'v t = 'v list (* a conjunction over all the elements in the list *)
 
-let create_pathcondition : ('v list) = []
+let create_pathcondition : ('v list) =
+  [ ]
 
 let add_condition (pc : 'v list) (t : 'v) : ('v list) =
   t::pc
 
-let string_of_pathcondition (to_string : 'v -> string) (pc : 'v list) : string = 
+let negate (pc : 'v list) : 'v =
+  List.fold_right (fun x y -> Expression.make_boperation Or (Expression.negate x) y) pc Expression.make_false
+
+let to_string (to_string : 'v -> string) (pc : 'v list) : string = 
   let rec aux pc : string list =
     match pc with
     | []      -> []
     | h :: t  -> (to_string h) :: (aux t)
-  in String.concat " ∧ " (aux pc)
+  in String.concat " AND " (aux pc) (* ∧ *)
 
-let print_pc (to_string : 'v -> string) (pc : 'v t) : unit =
-  string_of_pathcondition to_string pc |> print_endline
+let print (str : 'v -> string) (pc : 'v t) : unit =
+  to_string str pc |> print_endline
   
