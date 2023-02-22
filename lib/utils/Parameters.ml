ea8c9ca33417f0a0754ffcb5568f83d597f32fb6
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-22 22:28:19 +0000

    usual work

--- lib/utils/Parameters.ml
+++ lib/utils/Parameters.ml
@@ -1,7 +1,9 @@
 let main_id = "aenima" (*Ænima*)
 
-let symbol_prefix = "§_" (* Symbols X̂x̂ *)
+let symbol_prefix = "§__" (* Symbols X̂x̂§ *)
 
 let tank = 100000
 
 let size = 1000
+
+let max_int = 1000
