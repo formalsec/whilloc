66f08dd4250484e934a1fa29107eaff851fd7a8a
Francisco Miguel Sena <fmesena@gmail.com>
2023-02-22 22:29:42 +0000

    reorganizing prints and renaming variable names

--- lib/semantics/Return.ml
+++ lib/semantics/Return.ml
@@ -1,16 +1,10 @@
 type 'v t = 'v State.t * Outcome.t
 
-let string_of_return (to_string : 'v -> string) (ret : 'v t) : string =
+let string_of_return (str : 'v -> string) (ret : 'v t) : string =
   let (_,_,st,_,pc), out = ret in "\n#RETURN:\n" ^
-  " -Path cond.: " ^ (PathCondition.string_of_pathcondition to_string pc) ^ "\n" ^
-  " -Outcome   : " ^ (Outcome.string_of_outcome out)                      ^ "\n" ^
-  " -Store     : " ^ (Store.string_of_store to_string st)                 ^ "\n"
-
-let outcome (ret : 'v t) : Outcome.t =
-  let _, out = ret in out
-
-let pc (ret : 'v t) =
-  let (_,_,_,_,pc), _ = ret in pc
+  " -Outcome   : " ^ (Outcome.to_string out)                      ^ "\n" ^
+  " -Store     : " ^ (Store.to_string str st)                 ^ "\n" ^
+  " -Path cond.: " ^ (PathCondition.to_string str pc) ^ "\n" 
 
 let print (to_string : 'v -> string) (ret : 'v t) : unit =
   string_of_return to_string ret |> print_endline
