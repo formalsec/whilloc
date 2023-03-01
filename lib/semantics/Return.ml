7544a284241f0371c369574de48d8c47bba579b0
Francisco Miguel Sena <fmesena@gmail.com>
2023-03-01 18:13:58 +0000

    fix to_string

--- lib/semantics/Return.ml
+++ lib/semantics/Return.ml
@@ -1,10 +1,10 @@
 type 'v t = 'v State.t * Outcome.t
 
 let string_of_return (str : 'v -> string) (ret : 'v t) : string =
-  let (_,_,st,_,pc), out = ret in "\n#RETURN:\n" ^
-  " -Outcome   : " ^ (Outcome.to_string out)                      ^ "\n" ^
-  " -Store     : " ^ (Store.to_string str st)                 ^ "\n" ^
-  " -Path cond.: " ^ (PathCondition.to_string str pc) ^ "\n" 
+  let (_,_,st,_,pc), out = ret in "#RETURN:\n" ^
+  " -Outcome   : " ^ (Outcome.to_string out)          ^ "\n" ^
+  " -Store     : " ^ (Store.to_string str st)         ^ "\n" ^
+  " -Path cond.: " ^ (PathCondition.to_string str pc) ^ "\n\n" 
 
 let print (to_string : 'v -> string) (ret : 'v t) : unit =
   string_of_return to_string ret |> print_endline
