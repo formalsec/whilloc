61ffa489ee3909933a4b1f58fd7a99ca7d62f299
Francisco Miguel Sena <fmesena@gmail.com>
2023-03-08 19:24:27 +0000

    implements is_symbvalue predicate

--- lib/syntax/Value.ml
+++ lib/syntax/Value.ml
@@ -2,6 +2,11 @@ type t = Integer of int
        | Boolean of bool
        | SymbVal of string
 
+let is_symbolic_value (v : t) : bool =
+  match v with
+  | SymbVal _ -> true
+  | _         -> false
+
 let string_of_value (v : t) : string =
   match v with
   | Integer n -> "Int "     ^ (string_of_int n)
