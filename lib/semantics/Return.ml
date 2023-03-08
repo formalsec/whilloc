02b966d3b8d627c2be9cfeb7c21cda595c09a018
Francisco Miguel Sena <fmesena@gmail.com>
2023-03-08 19:25:30 +0000

    adds outcome and pc getter

--- lib/semantics/Return.ml
+++ lib/semantics/Return.ml
@@ -4,7 +4,15 @@ let string_of_return (str : 'v -> string) (ret : 'v t) : string =
   let (_,_,st,_,pc), out = ret in "#RETURN:\n" ^
   " -Outcome   : " ^ (Outcome.to_string out)          ^ "\n" ^
   " -Store     : " ^ (Store.to_string str st)         ^ "\n" ^
-  " -Path cond.: " ^ (PathCondition.to_string str pc) ^ "\n\n" 
+  " -Path cond.: " ^ (PathCondition.to_string str pc) ^ "\n\n"
+
+let get_outcome (ret : 'v t) : Outcome.t =
+  let _,out = ret in
+  out
+
+let get_pc (ret : 'v t) : 'v PathCondition.t =
+  let state,_ = ret in
+  State.get_pathcondition state
 
 let print (to_string : 'v -> string) (ret : 'v t) : unit =
   string_of_return to_string ret |> print_endline
