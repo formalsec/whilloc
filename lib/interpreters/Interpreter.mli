c83900be973ce846fc0108790063624c2c9cca8a
Francisco Miguel Sena <fmesena@gmail.com>
2023-03-08 19:29:23 +0000

    adds checkpoints via optional 'origin state' argument in the interpret function

--- lib/interpreters/Interpreter.mli
+++ lib/interpreters/Interpreter.mli
@@ -1,4 +1,4 @@
 module type M = sig
     type t
-    val interpret : Program.program -> t Return.t list
+    val interpret : Program.program -> ?origin:t State.t -> unit -> t Return.t list * t State.t list
 end
