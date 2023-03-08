f15dae9aa298b2018b7e07bc5232ca7b3e3bcd99
Francisco Miguel Sena <fmesena@gmail.com>
2023-03-08 19:35:09 +0000

    adds metadata to output file, conforms with new interpreter signature

--- bin/main.ml
+++ bin/main.ml
@@ -4,25 +4,28 @@ open Utils
 module S  = MakeInterpreter.M (EvalSymbolic.M) (DFS.M)
 module C  = MakeInterpreter.M (EvalConcrete.M) (DFS.M)
 module CC = MakeInterpreter.M (EvalConcolic.M) (DFS.M)
-    
+
 let rec concolic_loop (program : Program.program) (global_pc : Expression.t PathCondition.t) (outs : CC.t Return.t list) : CC.t Return.t list = 
 
   if Encoding.is_sat global_pc then
 
-    let model  = Encoding.get_model () in
-    let ()     = SymbMap.update model  in
+    let model   = Encoding.get_model ~print_model:true () in
+    let ()      = SymbMap.update model  in
 
-    let returns = CC.interpret program in
+    let returns,conts = CC.interpret program () in
+    
+    ignore conts;
 
     let return  = List.hd returns in
     let state,_ = return          in
 
-    let _,pc   = List.split (State.get_pathcondition state) in
-    let neg_pc = PathCondition.negate pc                    in
+    let _,pc    = List.split (State.get_pathcondition state) in
+    let neg_pc  = PathCondition.negate pc                    in
 
-    concolic_loop program (neg_pc :: global_pc) (return::outs)
+    concolic_loop program (neg_pc::global_pc) (return::outs)
   
   else
+    let _ = SymbMap.clear () in
     outs
 
 let main =
@@ -37,21 +40,21 @@ let main =
   else
 
   let program   = !file |> read_file |> parse_program |> create_program in
-  let meta_data = Printf.sprintf ("Input file: %s\nExecution mode: %s\nOutput file: %s\n\n") !file !mode !out in 
+  let meta_data = Printf.sprintf ("Input file: %s\nExecution mode: %s\nOutput file: %s\n\n") !file !mode !out in
 
   let str_of_returns =
   match !mode with
 
-    | "c"   -> let returns        = C.interpret program in
+    | "c"   -> let returns,_ = C.interpret program () in
                String.concat "\n" (List.map (Return.string_of_return EvalConcrete.M.to_string) returns)
              
-    | "s"   -> let returns        = S.interpret program in
+    | "s"   -> let returns,_ = S.interpret program () in
                String.concat "\n" (List.map (Return.string_of_return EvalSymbolic.M.to_string) returns)
     
-    | "cc"  -> let returns        = concolic_loop program [ ] [ ] in
+    | "cc"  -> let returns   = concolic_loop program [ ] [ ] in
                String.concat "\n" (List.map (Return.string_of_return EvalConcolic.M.to_string) returns)
              
-    | _   -> invalid_arg "Unknown provided mode. Available modes are:\n  s : for symbolic interpretation\n  c : for concrete interpretation\n"
+    | _   -> invalid_arg "Unknown provided mode. Available modes are:\n  c : for concrete interpretation\n  s : for symbolic interpretation\n  cc : for concolic interpretation"
 
   in write_file !out (meta_data ^ str_of_returns);
   print_string "\n=====================\n\tExiting\n=====================\n\n"
