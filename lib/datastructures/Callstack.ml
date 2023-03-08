db953b87e65d4ddf495c489b6b82fc5545e1e7af
Francisco Miguel Sena <fmesena@gmail.com>
2023-03-08 19:30:39 +0000

    implements clone and to_string functions

--- lib/datastructures/Callstack.ml
+++ lib/datastructures/Callstack.ml
@@ -8,13 +8,36 @@ let create_callstack : 'v t =
 
 let top (cs : 'v t): 'v frame =
   match cs with
-  | [] -> raise (EmptyStack "Callstack.ml: tried to peek from an empty stack")
+  | [] -> raise (EmptyStack "Callstack.top: tried to peek from an empty stack")
   | top :: _ -> top
 
 let pop (cs : 'v t) : 'v t =
   match cs with
-  | [] -> raise (EmptyStack "Callstack.ml: tried to pop from an empty stack")
+  | [] -> raise (EmptyStack "Callstack.pop: tried to pop from an empty stack")
   | _ :: t -> t
 
 let push (cs : 'v t) (f : 'v frame) : 'v t =
   f::cs
+
+let rec dup (cs : 'v t) : 'v t =
+  match cs with
+  | []   -> failwith "InternalError: Callstack.dup, empty callstack"
+  | [h]  -> assert(h=Toplevel); [h]
+  | h::t -> (match h with
+            | Toplevel -> [h]
+            | Intermediate (store, cont, var) -> Intermediate (Store.dup store, cont, var) :: (dup t)
+            )
+
+let to_string (str : 'v -> string) (cs : 'v t) : string =
+  let f = fun x y -> 
+    match x with
+    | Toplevel -> "_Toplevel_\n" ^ y
+    | Intermediate (store, cont, var) -> "_Intermediate_:\n\t" ^
+                                        var ^ "\n\t\t" ^
+                                        (String.concat "\n\t\t" (List.map Program.string_of_stmt cont)) ^ "\n\t" ^
+                                        (Store.to_string str store) ^ "\n" ^ y
+  in
+  List.fold_right f cs ""
+
+let print (str : 'v -> string) (cs : 'v t) : unit =
+  to_string str cs |> print_endline
