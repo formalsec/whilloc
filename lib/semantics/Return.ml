cdcfb0b6f68a97e67c1be88ac806d7a3937dc246
Francisco Miguel Sena <fmesena@gmail.com>
2023-04-16 10:19:28 +0100

    updates return with 'h

--- lib/semantics/Return.ml
+++ lib/semantics/Return.ml
@@ -1,18 +1,18 @@
-type 'v t = 'v State.t * Outcome.t
+type ('v,'h) t = ('v,'h) State.t * Outcome.t
 
-let string_of_return (str : 'v -> string) (ret : 'v t) : string =
-  let (_,_,st,_,pc), out = ret in "#RETURN:\n" ^
+let string_of_return (str : 'v -> string) (ret : ('v,'h) t) : string =
+  let (_,_,st,_,pc,_), out = ret in "#RETURN:\n" ^
   " -Outcome   : " ^ (Outcome.to_string out)          ^ "\n" ^
   " -Store     : " ^ (Store.to_string str st)         ^ "\n" ^
   " -Path cond.: " ^ (PathCondition.to_string str pc) ^ "\n\n"
 
-let get_outcome (ret : 'v t) : Outcome.t =
+let get_outcome (ret : ('v,'h) t) : Outcome.t =
   let _,out = ret in
   out
 
-let get_pc (ret : 'v t) : 'v PathCondition.t =
+let get_pc (ret : ('v,'h) t) : 'v PathCondition.t =
   let state,_ = ret in
   State.get_pathcondition state
 
-let print (to_string : 'v -> string) (ret : 'v t) : unit =
+let print (to_string : 'v -> string) (ret : ('v,'h) t) : unit =
   string_of_return to_string ret |> print_endline
