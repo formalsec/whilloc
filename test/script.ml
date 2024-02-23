3abdec3abd3b663cbaf874521daca43fa874a99c
julianayang777 <julianayang2011@gmail.com>
2024-02-23 18:58:06 +0000

    add script for test

--- test/script.ml
+++ test/script.ml
@@ -1,5 +1,8 @@
 open Whilloc
-(* open Bos_setup *)
+open Bos
+
+exception Timeout
+
 module SAF_Choice = ListChoice.Make (EvalSymbolic.M) (HeapArrayFork.M)
 module SAITE_Choice = ListChoice.Make (EvalSymbolic.M) (HeapArrayITE.M)
 module ST_Choice = ListChoice.Make (EvalSymbolic.M) (HeapTree.M)
@@ -7,29 +10,14 @@ module SOPL_Choice = ListChoice.Make (EvalSymbolic.M) (HeapOpList.M)
 
 module SAF =
   Interpreter.Make (EvalSymbolic.M) (DFS.M) (HeapArrayFork.M) (SAF_Choice)
-
 module SAITE =
   Interpreter.Make (EvalSymbolic.M) (DFS.M) (HeapArrayITE.M) (SAITE_Choice)
-
-module ST = Interpreter.Make (EvalSymbolic.M) (DFS.M) (HeapTree.M) (ST_Choice)
-
+module ST = 
+	Interpreter.Make (EvalSymbolic.M) (DFS.M) (HeapTree.M) (ST_Choice)
 module SOPL =
   Interpreter.Make (EvalSymbolic.M) (DFS.M) (HeapOpList.M) (SOPL_Choice)
 
-(* TODO:
-  - receberes uma diretoria do teu Sys.argv.(2)
-  - traverse da diretoria e encontrar '*.wl'
-  - List.iter run_test_file
-  *)
-
-(*
-  Bos: https://erratique.ch/software/bos/doc/
- *)
-
-let _max_timeout = 10
-(*
-  timeout:
-
+let _max_timeout = 10.0
 let unset () = Sys.set_signal Sys.sigalrm Sys.Signal_ignore
 
 let set =
@@ -38,40 +26,71 @@ let set =
     Sys.set_signal Sys.sigalrm (Sys.Signal_handle raise);
     ignore
     @@ ( Unix.setitimer Unix.ITIMER_REAL
-           { Unix.it_interval = 0.; Unix.it_value = _max_timeout }
-         : Unix.interval_timer_status )
+          { Unix.it_interval = 0.; Unix.it_value = _max_timeout}
+        : Unix.interval_timer_status )
 
-let run_test_file model file : 'a Result.t =
+let run model file = 
+  print_string "\n=====================\n\tÆnima\n=====================\n\n";
+  let program = file |> Utils.read_file |> Utils.parse_program |> Utils.create_program in
+    Printf.printf "Input file: %s\nExecution mode: %s\n\n" file model;
+    (match model with
+    | "saf" ->
+        let rets = SAF.interpret program in
+        List.iter
+          (fun (out, _) ->
+            Format.printf "Outcome: %s@." (Outcome.to_string out))
+          rets
+    | "saite" ->
+        let rets = SAITE.interpret program in
+        List.iter
+          (fun (out, _) ->
+          Format.printf "Outcome: %s@." (Outcome.to_string out))
+        rets
+    | "st" ->
+      let rets = ST.interpret program in
+      List.iter
+        (fun (out, _) ->
+        Format.printf "Outcome: %s@." (Outcome.to_string out))
+      rets
+    | "sopl" ->
+      let rets = SOPL.interpret program in
+      List.iter
+        (fun (out, _) ->
+        Format.printf "Outcome: %s@." (Outcome.to_string out))
+      rets
+    | _ -> assert false)
+
+let run_with_timeout model file =
   try
     Fun.protect ~finally:unset (fun () ->
         set ();
         try
-            Format.printf "Running '%s'@." input_file;
-            let program =
-              Utils.read_file input_file |> Utils.parse_program |> Utils.create_program
-            in
-            match model with
-            | "saite" ->
-                let outcomes = SAITE.interpret program in
-                List.iter
-                  (fun (o, _) -> Format.printf "Outcome: %s@." (Outcome.to_string o))
-                  outcomes
-            | _ -> assert false
-        with Timeout -> print "file %s timeout" )
-  with Timeout -> print "General timeout?"
-*)
+          run model file
+        with Timeout -> Printf.printf "File %s timeout" (file))
+  with Timeout -> Printf.printf "General timeout"
 
-let () =
+let () = 
   let model = Sys.argv.(1) in
-  let input_file = Sys.argv.(2) in
-  Format.printf "Running '%s'@." input_file;
-  let program =
-    Utils.read_file input_file |> Utils.parse_program |> Utils.create_program
+	let dir = Fpath.v Sys.argv.(2) in
+  let sum = 0 in
+  let result =
+    OS.Path.fold
+      ~elements:`Files
+      ~traverse:`Any
+      ((fun model file sum -> 
+        if Fpath.has_ext ".wl" file then 
+        (Printf.printf "Processing: %s\n" (Fpath.to_string file);
+        (run_with_timeout model (Fpath.to_string file));
+        sum + 1)
+      else
+        (Printf.printf "Skipped: %s\n" (Fpath.to_string file);
+        sum)) 
+      model)
+      sum
+      [dir]
   in
-  match model with
-  | "saite" ->
-      let outcomes = SAITE.interpret program in
-      List.iter
-        (fun (o, _) -> Format.printf "Outcome: %s@." (Outcome.to_string o))
-        outcomes
-  | _ -> assert false
+  match result with
+  | Ok sum ->
+      Printf.printf "Total number of files tested: %d\n" sum
+  | Error _ ->
+      Printf.printf "Error during fold: \n"
